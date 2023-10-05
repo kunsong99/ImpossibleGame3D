114c38861c7e22e23f2dd04bed0c54f087c8feb7
somrar <kun.song@student.forsbergsskola.se>
2023-10-05 11:41:58 +0200

    Add Palyer Jump
    add more traps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.04f;
+    public float defaultVelocityY = 100.0f;
+    public float defaultJumpForceY = 300.0f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+        float velocityY = rigidBody.velocity.y;
+        
+        if (velocityY < -0.1f)
+        {
+            velocityY = defaultVelocityY;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        //Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
-        //rigidBody.AddForce(0,10f,0);
         transform.Translate(0,0,speed);
+        //these two ways both work
+        //if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if(Input.GetButtonDown("Jump"))
+        {
+            Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+            rigidBody.AddForce(0,defaultJumpForceY,0);
+        }
     }
 }
