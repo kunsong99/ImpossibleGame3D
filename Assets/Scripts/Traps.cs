4f1a33e3d5d31e93cfc8855f0174394837305e1c
somrar <kun.song@student.forsbergsskola.se>
2023-10-05 10:03:30 +0200

    Recover Trap Movement

--- Assets/Scripts/Traps.cs
+++ Assets/Scripts/Traps.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace DefaultNamespace
+public class Traps : MonoBehaviour
 {
-    public class Traps : MonoBehaviour
+    private IEnumerator OnCollisionEnter(Collision other)
     {
-        
+        Destroy(other.gameObject);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(0);
     }
 }
