05b5262fab0807c4e6e69d5a197f8465cd0cbc09
somrar <kun.song@student.forsbergsskola.se>
2023-10-05 14:05:01 +0200

    Add Hover Effect on Player

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class StartGameButton : MonoBehaviour
+{
+    public void HoverStart()
+    {
+        transform.localScale = Vector3.one * 1.1f;
+    }
+
+    public void HoverEnd()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public void ClickStart()
+    {
+        
+    }
+
+    public void ClickEnd()
+    {
+        
+    }
+
+    public void Click()
+    {
+        
+    }
+    
+}
