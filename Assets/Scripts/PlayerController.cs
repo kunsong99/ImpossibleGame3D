1ec28b873b62d5d99fb95851f0527be0817f93c0
somrar <kun.song@student.forsbergsskola.se>
2023-10-05 10:59:21 +0200

    Add Bouncy Physic Material to Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+        //rigidBody.AddForce(0,10f,0);
         transform.Translate(0,0,speed);
     }
 }
